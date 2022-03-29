754f595d9b29f7d9976873c805b7fb16e2f0a38b
Tony Lee <tonylee@R2Hs-MBP.lan>
2022-03-28 22:52:39 -0400

    changed ports

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.get("/api/products", (req, res) => {
     });
 });
 
-app.listen(4000, () => {
-    console.log(`console server listening on port 4000`)
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
